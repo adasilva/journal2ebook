77f34a1f7a559ca5a437111f4553c2621c8b1bc2
adasilva <awesomeashley527@gmail.com>
2014-01-02 15:24:42 -0600

    add linux setup and install files

---
+++ src/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='journal2ebook',
+      version='0.1.1',
+      scripts = ['journal2ebook'],
+      description='Conversion tool for academic pdfs',
+      author='Ashley DaSilva and Jason Gullifer',
+      author_email='adasilva@physics.utexas.edu',
+      url='https://github.com/adasilva/journal2ebook',
+      license='LICENSE',
+     )
